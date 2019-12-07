53b739a70471394675ced64407cf7dc232536233
Facundo Cuassolo <facu.cuassolo.95@gmail.com>
2019-12-06 22:51:02 -0300

    se agrega hooks de form

--- client/src/signup.js
+++ client/src/signup.js
@@ -1,15 +1,24 @@
 import React from 'react';
+import useSingUpForm from './signupHooks'
 
 const Signup = () => {
+
+    const signup = () => {
+        alert(`login with this data:
+                username:${inputs.username}
+                password:${inputs.password}`)
+    }
+
+    const {inputs,handleSubmit,handleInputChange} = useSingUpForm(signup)
     return(
-        <form>
+        <form onSubmit={handleSubmit}>
             <div>
                 <label>Username</label>
-                <input type="text" name="username" required />
+                <input type="text" name="username" onChange={handleInputChange} value={inputs.username} required />
             </div>
             <div>
                 <label>Password</label>
-                <input type="password" name="password" required />
+                <input type="password" name="password" onChange={handleInputChange} value={inputs.password} required />
             </div>
             <button type="submit">Sing Up</button>
         </form>
