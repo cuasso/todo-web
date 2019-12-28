d6d2aa15af96f9765fc281210080be2cfffa6a22
Facundo Cuassolo <facu.cuassolo.95@gmail.com>
2019-12-28 19:53:52 -0300

    Se genera logica basica para autenticar usuarios

--- client/src/routes/index.jsx
+++ client/src/routes/index.jsx
@@ -1,18 +1,39 @@
-import React from 'react'
-import { Route, BrowserRouter as Router, Switch } from 'react-router-dom'
+import React, { Component, useContext } from 'react'
+import { Route, BrowserRouter as Router, Switch, Redirect } from 'react-router-dom'
+import createBrowserHistory from 'history'
+import { getSessionCookie, SessionContext } from '../session'
 import Signin from '../components/Signin'
 import Home from '../components/Home'
 import NotFound from '../components/NotFound'
 
+
+const [session, setSession] = useState(getSessionCookie());
+useEffect(
+    () => {
+        setSession(getSessionCookie());
+    },
+    [session]
+);
+
+const history = createBrowserHistory()
+const PrivateRoute = ({ component: Component, ...rest }) => (
+    <Route {...rest} render={(props) => (
+        getSessionCookie() === undefined
+            ? <Component {...props} />
+            : <Redirect to='/login' />
+    )} />)
+
 const Routing = () => {
     return (
-        <Router>
-            <Switch>
-                <Route exact path="/" component={Home} />
-                <Route path="/login" component={Signin} />
-                <Route component={NotFound} />
-            </Switch>
-        </Router>
+        <SessionContext.Provider value={session} >
+            <Router history={history}>
+                <Switch>
+                    <Route path="/login" component={Signin} />
+                    <PrivateRoute exact path="/" component={Home} />
+                    <Route component={NotFound} />
+                </Switch>
+            </Router>
+        </SessionContext.Provider>
     )
 }
 
---
+++ client/src/session.js
@@ -0,0 +1,12 @@
+import Cookies from 'js-cookie';
+
+const setSessionCookie = (session) => {
+    Cookies.remove('session')
+    Cookies.set('session', session, { expires: 14 })
+}
+
+const getSessionCookie = () => { return Cookies.get('session') }
+
+const SessionContext = React.createContext(getSessionCookie())
+
+export default { setSessionCookie, getSessionCookie, SessionContext }
